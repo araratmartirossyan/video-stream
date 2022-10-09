5f16802450a85fa35bacb19edc0d801375a84d95
Ararat <ararat.m.nl.icwt@gmail.com>
2022-10-09 13:38:16 +0200

    feat(part4): added video stream player

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import logger from 'morgan'
+import * as path from 'path'
+
+// routes
+import streamRouter from './modules/stream/stream.controller'
+import contentRouter from './modules/content/content.controller'
+
+// middleware
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+// endpoints
+app.use('/stream', streamRouter)
+app.use('/content', contentRouter)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+  console.log('Начинаем стримить')
+  console.log(`http://localhost:${PORT}`)
+})
